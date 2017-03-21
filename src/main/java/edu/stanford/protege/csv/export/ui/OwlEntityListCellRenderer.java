fa9267be2475ec46e310d9eba593ef400685eafa
Bob Dionne <dionne@dionne-associates.com>
2017-03-21 09:47:25 -0400

    Integrate csv exporter plugin

---
+++ src/main/java/edu/stanford/protege/csv/export/ui/OwlEntityListCellRenderer.java
@@ -0,0 +1,38 @@
+package edu.stanford.protege.csv.export.ui;
+
+import org.protege.editor.owl.OWLEditorKit;
+import org.protege.editor.owl.ui.renderer.OWLCellRenderer;
+import org.semanticweb.owlapi.model.OWLEntity;
+
+import javax.annotation.Nonnull;
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * @author Rafael Gonçalves <br>
+ * Center for Biomedical Informatics Research <br>
+ * Stanford University
+ */
+public class OwlEntityListCellRenderer extends DefaultListCellRenderer {
+    private static final long serialVersionUID = 5153646254115550535L;
+    private IncQualsOWLCellRenderer owlCellRenderer;
+
+    public OwlEntityListCellRenderer(@Nonnull OWLEditorKit editorKit,
+    		Map<OWLEntity, List<OWLEntity>> depMap) {
+        owlCellRenderer = new IncQualsOWLCellRenderer(editorKit, depMap);
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        Component label = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        if(value instanceof ExportDialogPanel.OwlEntityListItem) {
+            OWLEntity entity = ((ExportDialogPanel.OwlEntityListItem) value).getEntity();
+            label = owlCellRenderer.getListCellRendererComponent(list, entity, index, isSelected, cellHasFocus);
+        }
+        return label;
+    }
+}
